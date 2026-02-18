fdf516a550c0b2b77c6564b307064f3dbb5fc3dd
Halit Enes Kalaycı <halitenes.kalayci@gmail.com>
2026-02-18 10:25:45 +0300

    first commit

---
+++ identity.service/src/main/java/com/example/identity/service/entity/User.java
@@ -0,0 +1,63 @@
+package com.example.identity.service.entity;
+
+import jakarta.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true, length = 11)
+    private String tckn;
+
+    public User() {
+    }
+
+    public User(String username, String password, String tckn) {
+        this.username = username;
+        this.password = password;
+        this.tckn = tckn;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTckn() {
+        return tckn;
+    }
+
+    public void setTckn(String tckn) {
+        this.tckn = tckn;
+    }
+}
